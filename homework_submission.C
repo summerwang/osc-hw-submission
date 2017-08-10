8a2009f22b4879470fa5648fe4650c36f17b7ebb
Summer Wang <xwang@osc.edu>
2017-08-10 10:19:09 -0400

    call .sh in /fs/scratch/xwang/submit_hw

--- homework_submission.C
+++ homework_submission.C
@@ -6,8 +6,10 @@ int main ()
 {
 
 system("/fs/scratch/xwang/submit_hw/homework_submission.sh");
+
 /*
 system("/users/oscgen/xwang/scripts/Submit_Homework/osc-hw-submission/homework_submission.sh");
 */
+
 return 0;
 }
